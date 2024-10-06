d83dfa48856bc5b24ac82d2e5db208f3e16c8da5
louisjoety <kolouis002@gmail.com>
2024-10-06 23:11:33 +0800

    Make testing easier

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -8,18 +8,15 @@ import java.io.IOException;
 import java.util.Set;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException { // Declare IOException in the method signature
         String filePath = "C:\\Users\\Louis Joe\\IdeaProjects\\tp\\data\\database.json"; // Change this to your actual file path
 
-        try (JsonReader jsonReader = Json.createReader(new FileReader(filePath))) {
-            JsonObject jsonObject = jsonReader.readObject();
+        JsonReader jsonReader = Json.createReader(new FileReader(filePath)); // No try-catch block
+        JsonObject jsonObject = jsonReader.readObject();
 
-            Set<String> universityNames = jsonObject.keySet();
-            for (String universityName : universityNames) {
-                System.out.println(universityName);
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading the JSON file: " + e.getMessage());
+        Set<String> universityNames = jsonObject.keySet();
+        for (String universityName : universityNames) {
+            System.out.println(universityName);
         }
     }
 }
