21182dc6e2930729cb388546ba1200b4bd165935
louisjoety <kolouis002@gmail.com>
2024-10-06 23:06:01 +0800

    Allow data transfer from database

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,51 +3,23 @@ package seedu.duke;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
-import java.io.StringReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Set;
 
 public class Duke {
     public static void main(String[] args) {
-        String jsonString = "{\n" +
-                "  \"Chulalongkorn University\": {\n" +
-                "    \"courses\": [\n" +
-                "      {\n" +
-                "        \"pu_course_code\": \"ICE2190472\",\n" +
-                "        \"pu_course_name\": \"Netcentric Architecture\",\n" +
-                "        \"nus_course_code\": \"CS2105\",\n" +
-                "        \"nus_course_name\": \"Introduction to Computer Networks\"\n" +
-                "      }\n" +
-                "    ]\n" +
-                "  },\n" +
-                "  \"The University of Melbourne\": {\n" +
-                "    \"courses\": [\n" +
-                "      {\n" +
-                "        \"pu_course_code\": \"COMP30019\",\n" +
-                "        \"pu_course_name\": \"Graphics and Interaction\",\n" +
-                "        \"nus_course_code\": \"CS3241\",\n" +
-                "        \"nus_course_name\": \"Computer Graphics\"\n" +
-                "      }\n" +
-                "    ]\n" +
-                "  },\n" +
-                "  \"The Australian National University\": {\n" +
-                "    \"courses\": [\n" +
-                "      {\n" +
-                "        \"pu_course_code\": \"COMP3670\",\n" +
-                "        \"pu_course_name\": \"Introduction to Machine Learning\",\n" +
-                "        \"nus_course_code\": \"CS3244\",\n" +
-                "        \"nus_course_name\": \"Machine Learning\"\n" +
-                "      }\n" +
-                "    ]\n" +
-                "  }\n" +
-                "}";
+        String filePath = "C:\\Users\\Louis Joe\\IdeaProjects\\tp\\data\\database.json"; // Change this to your actual file path
 
-        try (JsonReader jsonReader = Json.createReader(new StringReader(jsonString))) {
+        try (JsonReader jsonReader = Json.createReader(new FileReader(filePath))) {
             JsonObject jsonObject = jsonReader.readObject();
 
             Set<String> universityNames = jsonObject.keySet();
             for (String universityName : universityNames) {
                 System.out.println(universityName);
             }
+        } catch (IOException e) {
+            System.err.println("Error reading the JSON file: " + e.getMessage());
         }
     }
 }
