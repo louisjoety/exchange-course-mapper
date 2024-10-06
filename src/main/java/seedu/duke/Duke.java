b4e9564e6fa64af08d84e61fc20692ad70217c8d
louisjoety <kolouis002@gmail.com>
2024-10-06 22:55:59 +0800

    Use dummy data to experiment on new dependency

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,53 @@
 package seedu.duke;
 
-import java.util.Scanner;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.StringReader;
+import java.util.Set;
 
 public class Duke {
-    /**
-     * Main entry-point for the java.duke.Duke application.
-     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+        String jsonString = "{\n" +
+                "  \"Chulalongkorn University\": {\n" +
+                "    \"courses\": [\n" +
+                "      {\n" +
+                "        \"pu_course_code\": \"ICE2190472\",\n" +
+                "        \"pu_course_name\": \"Netcentric Architecture\",\n" +
+                "        \"nus_course_code\": \"CS2105\",\n" +
+                "        \"nus_course_name\": \"Introduction to Computer Networks\"\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  \"The University of Melbourne\": {\n" +
+                "    \"courses\": [\n" +
+                "      {\n" +
+                "        \"pu_course_code\": \"COMP30019\",\n" +
+                "        \"pu_course_name\": \"Graphics and Interaction\",\n" +
+                "        \"nus_course_code\": \"CS3241\",\n" +
+                "        \"nus_course_name\": \"Computer Graphics\"\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  \"The Australian National University\": {\n" +
+                "    \"courses\": [\n" +
+                "      {\n" +
+                "        \"pu_course_code\": \"COMP3670\",\n" +
+                "        \"pu_course_name\": \"Introduction to Machine Learning\",\n" +
+                "        \"nus_course_code\": \"CS3244\",\n" +
+                "        \"nus_course_name\": \"Machine Learning\"\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  }\n" +
+                "}";
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        try (JsonReader jsonReader = Json.createReader(new StringReader(jsonString))) {
+            JsonObject jsonObject = jsonReader.readObject();
+
+            Set<String> universityNames = jsonObject.keySet();
+            for (String universityName : universityNames) {
+                System.out.println(universityName);
+            }
+        }
     }
 }
